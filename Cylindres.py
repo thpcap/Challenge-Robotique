69fe83714c46e4969a64c67171755506f4e49153
thpcap <126089066+thpcap@users.noreply.github.com>
2025-01-27 09:13:43 +0100

    First Commit started Cylyndre.py

---
+++ Cylindres.py
@@ -0,0 +1,14 @@
+
+class Cylindres:
+    def __init__(self,Id,Rayon,x,y,Masse,Recompense):
+        self.Id=Id
+        self.Rayon=Rayon
+        self.x=x
+        self.y=y
+        self.Masse=Masse
+        self.Recompense=Recompense
+    def Distance(self,Robot):
+        r1=self.Rayon
+        r2=Cylindre.Rayon
+        
+        
