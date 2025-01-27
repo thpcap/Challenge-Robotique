28638421ab3899d11afba05d01ecd834ca8f07d6
thpcap <126089066+thpcap@users.noreply.github.com>
2025-01-27 12:01:22 +0100

    added file writing in Calcul.py

--- Calcul.py
+++ Calcul.py
@@ -13,26 +13,34 @@ def h(robot, cylindre):
     return robot.distance(cylindre)*weights["distance"] + cylindre.Recompense * weights["reward"] + cylindre.Masse*weights["mass"]
 
 def path(robot, cylindres):
-    for Cyl in cylindres:
-        print(Cyl)
-    robot = Robot()
-    path = []
+    with open("output.txt",mode="w")as Output_File:
+        Output_Str=""
+        for Cyl in cylindres:
+            print(Cyl)
+        robot = Robot()
+        path = []
 
-    while robot.fuel > 0:
-        best = Cylindres()
-        best_value = 0
-        for cylindre in cylindres:
-            value = h(robot, cylindre)
-            if best_value < value:
-                best_value = value
-                best = cylindre
-        path.append(best)
+        while robot.fuel > 0:
+            best = Cylindres()
+            best_value = 0
+            for cylindre in cylindres:
+                value = h(robot, cylindre)
+                if best_value < value:
+                    best_value = value
+                    best = cylindre
+            path.append(best)
 
-        dist = robot.distance(best)
-        robot.orientation += robot.angle(cylindre)
-        robot.fuel = robot.consumption * dist
-        robot.mass += best.Masse
-        robot.x = math.cos(math.pi/2 - angle) * dist
-        robot.y = math.sin(math.pi/2 - angle) * dist
-
-    return path
+            dist = robot.distance(best)
+            robot.orientation += robot.angle(cylindre)
+            robot.fuel = robot.consumption * dist
+            robot.mass += best.Masse
+            robot.x = math.cos(math.pi/2 - angle) * dist
+            robot.y = math.sin(math.pi/2 - angle) * dist
+            
+            #add Commands for the Robot
+            Output_Str+="TURN "+str(robot.angle(cylindre))+"\n"
+            Output_Str+="GO"+str(dist)+"\n"
+            
+        Output_Str+="FINISH"
+        Output_File.write(Output_Str)
+        Output_File.close()
