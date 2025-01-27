55b093a648a1ab6e0fc6269d99a64fe4bf05296d
thpcap <126089066+thpcap@users.noreply.github.com>
2025-01-27 11:32:41 +0100

    added mass generation and values for cylyndres

--- Cylindres.py
+++ Cylindres.py
@@ -5,8 +5,21 @@ class Cylindres:
         self.x=x
         self.y=y
         self.Type=Type
+        if(self.Type==1):
+            self.Masse=1
+            self.Valeur=1
+        elif(self.Type==2):
+            self.Masse=2
+            self.Valeur=2
+        elif(self.Type==3):
+            self.Masse=2
+            self.Valeur=3
+        else:
+            print("type de Cylindre non reconnu")
+            self.Masse=-1
+            self.Valeur=-1
     
     def __str__(self):
-        return "id="+str(self.Id)+" x="+str(self.x)+" y="+str(self.y)+" Type="+str(self.Type)
+        return "id="+str(self.Id)+" x="+str(self.x)+" y="+str(self.y)+" Type="+str(self.Type)+" masse="+str(self.Masse)+" valeur="+str(self.Valeur)
         
         
