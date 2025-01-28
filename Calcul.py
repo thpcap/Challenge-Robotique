e53bea85fb7243e7acdbeb4c025ae7918e691312
thpcap <126089066+thpcap@users.noreply.github.com>
2025-01-28 11:19:02 +0100

    adjust fuel parameters and improve pathfinding visualization in Calcul.py and Robot.py

--- Calcul.py
+++ Calcul.py
@@ -8,41 +8,41 @@ import math
 import matplotlib.pyplot as plt
 import matplotlib.patches as pat
 
-train=False
 
 weights={
     "distance": -2,
-    "reward": 10,
+    "reward": 8,
     "mass": -2
 }
 
 def h(robot, cylindre):
     return robot.Distance(cylindre)*weights["distance"] + cylindre.Valeur * weights["reward"] + cylindre.Masse*weights["mass"]
 
-def path(robot, cylindres):
+def path(robot, cylindres, train=False):
     #ouvre le fichier d'output
     with open(output_link,mode="w")as Output_File:
         Output_Str=""
         robot = Robot()
-        fig, ax = plt.subplots()
         reward_list = [0]
         mass_list = [0]
-        plt.xlim(-5,30)
-        plt.ylim(-5,25)
-        plt.grid(linestyle='--')
-        ax.set_aspect(1)
-        for Cyl in cylindres:
-            circle = plt.Circle((Cyl.x,Cyl.y ), Cyl.Rayon, color='black')
-            circle2 = plt.Circle((Cyl.x,Cyl.y ), Cyl.Rayon-0.1, color=Cyl.color)
-            ax.add_artist(circle)
-            ax.add_artist(circle2)
-        ax.add_artist(pat.Rectangle((-0.5, -0.5), 1, 1, color = 'black'))
-        ax.add_artist(pat.Rectangle((-0.4, -0.4), 0.8, 0.8, color = 'magenta'))
-        ax.plot((1,0),(0,0),color='magenta')
+        if(not train) : 
+            _, ax = plt.subplots()
+            plt.xlim(-5,30)
+            plt.ylim(-5,25)
+            plt.grid(linestyle='--')
+            ax.set_aspect(1)
+            for Cyl in cylindres:
+                circle = plt.Circle((Cyl.x,Cyl.y ), Cyl.Rayon, color='black')
+                circle2 = plt.Circle((Cyl.x,Cyl.y ), Cyl.Rayon-0.1, color=Cyl.color)
+                ax.add_artist(circle)
+                ax.add_artist(circle2)
+            ax.add_artist(pat.Rectangle((-0.5, -0.5), 1, 1, color = 'black'))
+            ax.add_artist(pat.Rectangle((-0.4, -0.4), 0.8, 0.8, color = 'magenta'))
+            ax.plot((1,0),(0,0),color='magenta')
 
         time = 0
         times = [0]
-        while (robot.fuel > 0 and len(cylindres)):
+        while (robot.fuel > 0 and len(cylindres) and time<=600):
             #calcule le meilleur cylindre à  atteindre
             best = cylindres[0]
             best_value = 0
@@ -77,29 +77,33 @@ def path(robot, cylindres):
             robot.y += (math.sin(math.pi/2 - robot.orientation) * dist)
             time += dist/robot.vitesse()
 
-            ax.plot((robot.x,x),(robot.y,y),color='black')
             times.append(time)
             reward_list.append(robot.reward)
             mass_list.append(robot.mass)
-            circle = plt.Circle((robot.x,robot.y ), 0.1, color='black')
-            ax.add_artist(circle)
+            if(not train) : 
+                ax.plot((robot.x,x),(robot.y,y),color='black')
+                circle = plt.Circle((robot.x,robot.y ), 0.1, color='black')
+                ax.add_artist(circle)
 
             #add Commands for the Robot
             Output_Str+="TURN "+str(-angl*(180/math.pi))+"\n"
             Output_Str+="GO "+str(dist)+"\n"
 
         plt.title('path '+str(robot.reward), fontsize=8)
-        fig2, axs = plt.subplots(2)
-        ax2=axs[0]
-        ax3=axs[1]
-        plt.title('reward/mass', fontsize=8)
-        ax2.plot(times, reward_list, color="blue")
-        ax3.plot(times, mass_list, 0, color="black")            
+        if(not train) : 
+            _, axs = plt.subplots(2)
+            ax2=axs[0]
+            ax3=axs[1]
+            plt.title('reward/mass', fontsize=8)
+            ax2.plot(times, reward_list, color="blue")
+            ax3.plot(times, mass_list, 0, color="black")            
+        
         Output_Str+="FINISH"
         Output_File.write(Output_Str)
         Output_File.close()
+        if(not train) : plt.show()
+        return robot.reward
         
-        plt.show()
 
 generate(30)
 cylindres=Input_Map(input_link)
