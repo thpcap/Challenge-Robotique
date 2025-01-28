8aab68b21d748f2134e4e0566cb5400f05ead541
Rayyze <leo@gmail.com>
2025-01-28 15:17:02 +0100

    train test 2, 10 maps

--- GenerateMap.py
+++ GenerateMap.py
@@ -1,9 +1,10 @@
 import random
+from Cylindres import *
 
 def distance(ax, ay, bx, by):
     return ((ax-bx)**2 + (ay-by)**2)**0.5
 
-def generateMap(cylinder_number, x_max, y_max, x_min, y_min, distance_threshold):
+def generateMap(cylinder_number, x_max = 25, y_max = 20, x_min = 0, y_min = 0, distance_threshold = 1):
     cylinder_list = []
     for i in range(cylinder_number):
         flag = True
@@ -20,9 +21,10 @@ def generateMap(cylinder_number, x_max, y_max, x_min, y_min, distance_threshold)
             temp_x = random.random() * x_max + x_min
             temp_y = random.random() * y_max + y_min
             for j in range(len(cylinder_list)):
-                if distance(cylinder_list[j][0], cylinder_list[j][1], temp_x, temp_y) < distance_threshold:
+                if distance(cylinder_list[j].x, cylinder_list[j].y, temp_x, temp_y) < distance_threshold:
                     flag = True
-        cylinder_list.append((temp_x, temp_y, float(int(random.random() * 3 + 1))))
+        cylinder = Cylindres(i, temp_x, temp_y, float(int(random.random() * 3 + 1)))
+        cylinder_list.append(cylinder)
     return cylinder_list
 
 def writeMap(cylinder_list):
@@ -32,7 +34,7 @@ def writeMap(cylinder_list):
 
     f = open("random_map.csv",mode="a")
     for cylinder in cylinder_list:
-        f.write(str(cylinder[0]) + "    " + str(cylinder[1]) + "    " + str(cylinder[2]) + "\n")
+        f.write(str(cylinder.x) + "    " + str(cylinder.y) + "    " + str(cylinder.Type) + "\n")
     f.close()
 
 def generate(cylinder_number, x_max = 25, y_max = 20, x_min = 0, y_min = 0, distance_threshold = 1):
